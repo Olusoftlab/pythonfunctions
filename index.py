cc1d959c7bf7c1799d8194ab2afb5c369925648b
olumild <olumild998@gmail.com>
2024-11-07 17:49:06 +0100

    working with functions in python

---
+++ index.py
@@ -0,0 +1,24 @@
+#function to calculate orihinal price by entrying price and discount price
+
+def calculate_discount_price(price, discount_percent):
+       
+        discount_price=price *discount_percent
+        original_price=price - discount_price
+        
+        print(original_price)
+    
+
+calculate_discount_price(20000, 0.005)
+
+
+input_price=int(input("Enter the original price: "))
+input_discount=input("Enter the discount price")
+
+if (input_discount==""):
+    print(input_price)
+else:
+    getDiscount=float(input_discount)
+    calculate_discount_price(input_price, getDiscount)   
+
+  
+    
